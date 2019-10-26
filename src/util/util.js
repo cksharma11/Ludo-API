46d02871d558855bbcf85c802a237eca60e8ebaa
Arif Ansari <ext_aansari@falabella.cl>
2019-10-26 17:21:36 +0530

    Add utility functions

--- src/util/util.js
+++ src/util/util.js
@@ -1,4 +1,34 @@
+const { routes } = require('../../config/route');
+
 const getCurrentGame = (req, gameId = req.body.gameId) =>
   req.app.games.getGame(gameId);
 
-module.exports = { getCurrentGame };
+const getCoinPositionIndex = (color, position) => {
+  const selectedRoute = routes[color];
+  return selectedRoute.findIndex((x) => x === position);
+};
+
+const getRouteNewValue = (color, index) => routes[color][index];
+
+const getRouteFirstValue = (color) => routes[color][0];
+
+const isValidCoinPosition = (coinNumber, diceValue) => {
+  const selectedCoin = this.coins.coins.find((x) => x.number === +coinNumber);
+  const { position } = selectedCoin;
+  if (!position && diceValue !== 6) {
+    return false;
+  }
+  const selectedCoinPositionIndex = getCoinPositionIndex(this.color, position);
+  const newPositionIndex = diceValue + selectedCoinPositionIndex;
+  const newPosition = getRouteNewValue(this.color, newPositionIndex);
+
+  return !!newPosition;
+};
+
+module.exports = {
+  getCurrentGame,
+  getCoinPositionIndex,
+  getRouteNewValue,
+  getRouteFirstValue,
+  isValidCoinPosition
+};
