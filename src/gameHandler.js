a0d85e07e6ddef4e300a1e67a76034b70c29f187
Arif Ansari <ext_aansari@falabella.cl>
2019-10-26 17:20:48 +0530

    Add phase in game handler

--- src/gameHandler.js
+++ src/gameHandler.js
@@ -1,17 +1,50 @@
 const { getCurrentGame } = require('./util/util');
+const { PHASE } = require('./util/constant');
+
+const getCoinsPosition = (currentGame) => {
+  const coinsPosition = [];
+  currentGame.players.forEach((player) => {
+    player.coins.coins.forEach((coin) => {
+      const { number, position } = coin;
+      coinsPosition.push({
+        color: player.color,
+        position,
+        id: number,
+        playerId: player.id
+      });
+    });
+  });
+  return coinsPosition;
+};
 
 const getGameData = (req, res) => {
   const currentGame = getCurrentGame(req);
+  currentGame.coinsPosition = getCoinsPosition(currentGame);
   res.send(currentGame);
 };
 
 const rollDice = (req, res) => {
   const currentGame = getCurrentGame(req);
-  currentGame.rollDice();
+  if (currentGame.phase === PHASE.ROLL_DICE) {
+    currentGame.rollDice();
+  }
+  res.send({});
+};
+
+const coinPositionHandler = (req, res) => {
+  const currentGame = getCurrentGame(req);
+  const { id, playerId } = req.body;
+  if (currentGame.phase === PHASE.PLACE_COIN) {
+    const currentPlayer = currentGame.getCurrentPlayer();
+    if (currentPlayer.id === +playerId) {
+      currentGame.changeCurrentPlayerCoinPosition(id);
+    }
+  }
   res.send({});
 };
 
 module.exports = {
   getGameData,
-  rollDice
+  rollDice,
+  coinPositionHandler
 };
