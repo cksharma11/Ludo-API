709cdd3c29b5698eddc4bb144af9d4cf4f333ba1
Chandan Kumar <cksharma122@gmail.com>
2019-10-27 00:51:29 +0530

    update tests for new rolldice model

--- __tests__/model/game.test.js
+++ __tests__/model/game.test.js
@@ -7,26 +7,6 @@ describe('Game', () => {
     expect(game.currentPlayerIndex).toBe(1);
   });
 
-  // it('should roll dice and update turn if dice value is other than 6', () => {
-  //   const game = new Game(1, 4);
-  //   const mockMath = Object.create(global.Math);
-  //   mockMath.random = () => 3;
-  //   global.Math = mockMath;
-  //   game.rollDice();
-  //   expect(game.currentPlayerIndex).toBe(1);
-  //   expect(game.diceValue).toBe(18); // As it's Math.ceil(Math.random() * 6), so 3*6 = 18
-  // });
-
-  it('should roll dice and should not update turn if dice value is than 6', () => {
-    const game = new Game(1, 4);
-    const mockMath = Object.create(global.Math);
-    mockMath.random = () => 1;
-    global.Math = mockMath;
-    game.rollDice();
-    expect(game.currentPlayerIndex).toBe(0);
-    expect(game.diceValue).toBe(6); // As it's Math.ceil(Math.random() * 6), so 3*6 = 18
-  });
-
   it('should add player to game', () => {
     const game = new Game(1, 4);
     const player = { name: 'Alex' };
@@ -43,5 +23,45 @@ describe('Game', () => {
     game.addPlayer(player2);
 
     expect(game.isStarted).toBeTruthy();
+  });
+
+  describe('rollDice', () => {
+    // As it's Math.ceil(Math.random() * 6), so 3*6 = 18
+    // As it's Math.ceil(Math.random() * 6), so 3*6 = 18
+    it('should not update the phase if rolled value is 6', () => {
+      const game = new Game(1, 4);
+      const mockMath = Object.create(global.Math);
+      mockMath.random = () => 1;
+      global.Math = mockMath;
+      expect(game.phase).toBe(0);
+
+      game.rollDice();
+
+      expect(game.phase).toBe(0);
+    });
+
+    it('shold update the phase if rolled value is not 6', () => {
+      const game = new Game(1, 4);
+      const mockMath = Object.create(global.Math);
+      mockMath.random = () => 0.5;
+      global.Math = mockMath;
+      expect(game.phase).toBe(0);
+
+      game.rollDice();
+
+      expect(game.phase).toBe(1);
+    });
+  });
+
+  describe('changeCurrentPlayerCoinPosition', () => {
+    xit('should update coin position if validCoinPosition', () => {
+      const game = new Game(1, 4);
+      const mockMath = Object.create(global.Math);
+      mockMath.random = () => 0.5;
+      global.Math = mockMath;
+
+      game.changeCurrentPlayerCoinPosition(1);
+      expect(game.players[0].coins.coins[1].position).toBe(3);
+    });
   });
 });
