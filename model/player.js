acd1f0f082c332a84be2bca10b29bf69e636e72e
Arif Ansari <ext_aansari@falabella.cl>
2019-10-26 17:22:30 +0530

    Refactor player model

--- model/player.js
+++ model/player.js
@@ -1,4 +1,9 @@
 const CoinSet = require('./coinSet');
+const {
+  getCoinPositionIndex,
+  getRouteNewValue,
+  getRouteFirstValue
+} = require('../src/util/util');
 
 class Player {
   constructor(id, name, color) {
@@ -8,8 +13,40 @@ class Player {
     this.coins = new CoinSet();
   }
 
+  eliminateCoin(coinPosition) {
+    const coin = this.coins.coins.find((x) => x.position === coinPosition);
+    coin.setPosition(0);
+  }
+
+  hasActiveCoin() {
+    return !!this.coins.getActiveCoins().length;
+  }
+
   setCoins(coins) {
     this.coins = coins;
+  }
+
+  setCoinPosition(coinNumber, diceValue = 2) {
+    const selectedCoin = this.coins.coins.find((x) => x.number === +coinNumber);
+    const { position } = selectedCoin;
+    if (position) {
+      const selectedCoinPositionIndex = getCoinPositionIndex(
+        this.color,
+        position
+      );
+
+      const newPositionIndex = diceValue + selectedCoinPositionIndex;
+      const newPosition = getRouteNewValue(this.color, newPositionIndex);
+      selectedCoin.setPosition(newPosition);
+    } else if (diceValue === 6) {
+      const firstValue = getRouteFirstValue(this.color);
+      selectedCoin.setPosition(firstValue);
+    }
+  }
+
+  getCoinPosition(coinNumber) {
+    const coin = this.coins.coins.find((x) => x.number === coinNumber);
+    return coin.getPosition();
   }
 }
 
