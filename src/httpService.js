4920003b77333e74bf391322a339e27b9559558a
Chandan Kumar <cksharma122@gmail.com>
2019-10-31 23:14:25 +0530

    Deploying to heroku: Add DB_URL

--- src/httpService.js
+++ src/httpService.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const { DB_PORT = 5050 } = process.env;
 
-const DB_HOST = process.env.DB_HOST || `http://localhost:${DB_PORT}`;
+const DB_HOST = process.env.DB_URL || `http://localhost:${DB_PORT}`;
 
 const makeCall = async (body, path) => {
   return axios.post(`${DB_HOST}${path}`, { ...body }).then((res) => res);
